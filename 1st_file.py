be468d8b68ad3cb25f373915e39428bc548f1210
leeloolee <yunscien@naver.com>
2020-09-06 20:41:43 +0900

    commit

--- 1st_file.py
+++ 1st_file.py
@@ -1,5 +1,6 @@
 # Transpose A matrix in Pytorch
 import torch
+
 ## https://www.aiworkbox.com/lessons/construct-a-pytorch-tensor
 
 
@@ -13,16 +14,43 @@ pt_matrix_ex = torch.Tensor(
 
 pt_matrix_ex.T
 
-pt_random_matrix_ex = torch.rand(3,3,3)
+pt_random_matrix_ex = torch.rand(3, 3, 3)
 
 torch.__version__
 
 ## Change Tensor Type
-x = torch.rand(3,3,3)
+x = torch.rand(3, 3, 3)
 print(x)
 type(x)
 type(x.float())
 type(x.double())
 type(x.int())
 
+## concat
+x = torch.rand(3, 2, 2)
+y = torch.rand(3, 2, 2)
+
+torch.cat((x, y), 0).shape  # 어떤 dimension으로 concate하는지
+
 ##
+
+x_2 = torch.rand(20, 3, 3)
+print(x_2)
+
+## elementwise multiplication
+random_tensor_one_ex = (torch.rand(2, 3, 4) * 10).int()
+random_tensor_two_ex = (torch.rand(2, 3, 4) * 10).int()
+random_tensor_one_ex * random_tensor_two_ex
+
+## import torch
+import torch
+import torchvision
+import torchvision.datasets as datasets
+
+cifar_trainset = datasets.CIFAR10(root= "./data", train=True,
+                 transform=None, target_transform=None,
+                 download=True)
+
+cifar_testset = datasets.CIFAR10(root= "./data", train=False,
+                 transform=None, target_transform=None,
+                 download=False)
